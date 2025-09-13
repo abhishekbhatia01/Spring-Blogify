5aaaea41a6d571d598cdc0b30f7f24b986606981
Amjad <157952698+AmjadMishal@users.noreply.github.com>
2025-09-13 23:50:21 +0530

    feat: Integrate @tanstack/react-query for blog data fetching and enhance BlogCard component

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
-const BlogCard = () => {
+const BlogCard = ({ title, author, date, imgUrl }) => {
   return (
-    <div className="p-2 cursor-pointer">
-      <img
-        src="https://images.unsplash.com/photo-1629904853716-f0bc54eea481?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-        className="object-cover w-full rounded-md"
-      />
-      <div className="flex flex-col">
-        <h1 className="text-2xl font-semibold">Title</h1>
-        <h2>Author</h2>
-        <h3 className="text-gray-400">28 Aug 2025</h3>
+    <div className="bg-white rounded-md overflow-hidden shadow h-92 flex flex-col p-2 cursor-pointer">
+      <img src={imgUrl} alt="" className="object-cover w-full h-65" />
+      <div className="flex flex-col flex-1 gap-1 mt-2">
+        <h1 className="text-lg font-semibold line-clamp-2">{title}</h1>
+        <h2 className="text-sm text-gray-600">{author}</h2>
+        <h3 className="text-xs text-gray-400">{date?.split("T")[0]}</h3>
       </div>
     </div>
   );
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import BlogCard from "../components/BlogCard";
-import Pagination from "../components/Pagination";
+import { useQuery } from "@tanstack/react-query";
+import api from "../config/api";
 
 const Blogs = () => {
+  const [page, setPage] = useState(0);
+  const [size, setSize] = useState(4);
+  const fetchBlogs = async (page, size) => {
+    try {
+      // Pass page and size as query params
+      const res = await api.get(`/posts?page=${page}&size=${size}`);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["posts", page, size],
+    queryFn: () => fetchBlogs(page, size),
+  });
+
+  if (isLoading) {
+    return <div className="p-5">Loading...</div>;
+  }
+  if (isError) {
+    return (
+      <div className="p-5 text-red-500">
+        Error: {error?.message || "Failed to load blogs."}
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col w-full">
       <section className="p-5">
@@ -32,16 +60,71 @@ const Blogs = () => {
       </section>
       <section className="p-5">
         <div className="grid grid-cols-3 max-lg:grid-cols-2 gap-1 max-sm:grid-cols-1">
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
-          <BlogCard />
+          {Array.isArray(data?.data?.content) &&
+            data.data.content.map((post) => (
+              <BlogCard
+                title={post.title}
+                author={post.name}
+                date={post.createdAt}
+                key={post.id}
+                imgUrl={post.imgUrl}
+              />
+            ))}
         </div>
       </section>
       <section className="flex items-center justify-center p-5">
-        <Pagination />
+        <div className="flex items-center justify-between w-full max-w-80 text-gray-500 font-medium">
+          <button
+            type="button"
+            aria-label="prev"
+            disabled={data.data.first}
+            onClick={() => setPage(page - 1)}
+            className="rounded-full border border-slate-300/80 disabled:cursor-not-allowed"
+          >
+            <svg
+              width="40"
+              height="40"
+              viewBox="0 0 40 40"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M22.499 12.85a.9.9 0 0 1 .57.205l.067.06a.9.9 0 0 1 .06 1.206l-.06.066-5.585 5.586-.028.027.028.027 5.585 5.587a.9.9 0 0 1 .06 1.207l-.06.066a.9.9 0 0 1-1.207.06l-.066-.06-6.25-6.25a1 1 0 0 1-.158-.212l-.038-.08a.9.9 0 0 1-.03-.606l.03-.083a1 1 0 0 1 .137-.226l.06-.066 6.25-6.25a.9.9 0 0 1 .635-.263Z"
+                fill="#475569"
+                stroke="#475569"
+                strokeWidth=".078"
+              />
+            </svg>
+          </button>
+
+          <span>
+            Page {page + 1} of {data?.data?.totalPages}
+          </span>
+
+          <button
+            type="button"
+            disabled={data.data.last}
+            aria-label="next"
+            className="rounded-full border border-slate-300/80 disabled:cursor-not-allowed"
+            onClick={() => setPage(page + 1)}
+          >
+            <svg
+              className="rotate-180"
+              width="40"
+              height="40"
+              viewBox="0 0 40 40"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M22.499 12.85a.9.9 0 0 1 .57.205l.067.06a.9.9 0 0 1 .06 1.206l-.06.066-5.585 5.586-.028.027.028.027 5.585 5.587a.9.9 0 0 1 .06 1.207l-.06.066a.9.9 0 0 1-1.207.06l-.066-.06-6.25-6.25a1 1 0 0 1-.158-.212l-.038-.08a.9.9 0 0 1-.03-.606l.03-.083a1 1 0 0 1 .137-.226l.06-.066 6.25-6.25a.9.9 0 0 1 .635-.263Z"
+                fill="#475569"
+                stroke="#475569"
+                strokeWidth=".078"
+              />
+            </svg>
+          </button>
+        </div>
       </section>
     </div>
   );
